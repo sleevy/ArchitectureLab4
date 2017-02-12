29c178e89137bd53b50b6db6b5af003639c73193
sleevy <chirocam@gmail.com>
2017-02-11 22:53:18 -0500

    Added Lab 2 classes, changed package to match new location

---
+++ app/src/main/java/com/example/cameronrhodes/lab3alu/Gate.java
@@ -0,0 +1,20 @@
+package com.example.cameronrhodes.lab3alu;
+
+public abstract class Gate {
+    /**
+     *
+     */
+    abstract public void print();
+
+    /**
+     * Sets output based on inputs and gate type
+     */
+    abstract public void execute();
+
+
+    /**
+     * @return
+     */
+    abstract public boolean getOutput();
+
+}
